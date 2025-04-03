28988a923c2e1a2f983f33e005ba4f5ccba5edff
sayolaw <105397283+sayolaw@users.noreply.github.com>
2025-04-03 16:21:58 -0400

    requiring auth for viewing products

--- routes/web.php
+++ routes/web.php
@@ -7,14 +7,14 @@ use App\Http\Controllers\LoginController;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::prefix("products")->group(function(){
+Route::prefix("products",)->middleware('auth')->group(function(){
     Route::get('/', [ProductController::class, 'index']);
     Route::get('/show/{id}', [ProductController::class, 'show']);
     Route::post('/', [ProductController::class, 'store'])->name('products.store');
     Route::put('/update/{id}', [ProductController::class, 'update'])->name('products.update');
     Route::delete('/delete/{id}', [ProductController::class, 'destroy'])->name('products.destroy');
     Route::get('/form/{id?}', [ProductController::class, 'getForm']);
-});
+})->middleware('auth');
 Route::post('/api/login',[LoginController::class,"authenticate"]);
 Auth::routes();
 
